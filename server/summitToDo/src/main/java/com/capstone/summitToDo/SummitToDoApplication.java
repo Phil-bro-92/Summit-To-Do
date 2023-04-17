8558777607f72b740b845c87b5ee39b4f18f5692
Phil Broadley <philbrocoding@hotmail.com>
2023-04-17 18:05:20 +0100

    Java spring server created

---
+++ server/summitToDo/src/main/java/com/capstone/summitToDo/SummitToDoApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.summitToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummitToDoApplication.class, args);
+	}
+
+}
