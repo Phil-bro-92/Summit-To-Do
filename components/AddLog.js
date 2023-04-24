cf26e5993cae706a63b5b42ec551ac76ca4f569d
Phil Broadley <philbrocoding@hotmail.com>
2023-04-24 10:26:45 +0100

    date input

--- components/AddLog.js
+++ components/AddLog.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, Button, Pressable, DatePickerIOS, Modal} from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Pressable,
+  DatePickerIOS,
+  Modal,
+} from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faCloud, faCloudRain, faSnowflake, faSun, faX } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faCloud,
+  faCloudRain,
+  faSnowflake,
+  faSun,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 
 const AddLog = () => {
   const [LogFormVisible, setLogFormVisible] = useState(true);
@@ -21,73 +34,48 @@ const AddLog = () => {
   };
 
   const handleSubmitDate = (date) => {
-    setChosenDate(String(date))
-    console.log(date)
-  }
+    setChosenDate(String(date));
+    console.log(date);
+  };
 
   return (
-		<View>
-			{LogFormVisible ? (
-				<View>
-					<TextInput placeholder="Leave a comment"></TextInput>
-					<View>
-						<Text>Weather:</Text>
-						<Pressable>
-							<FontAwesomeIcon icon={faSun} size={15} />
-						</Pressable>
-						<Pressable>
-							<FontAwesomeIcon icon={faCloud} size={15} />
-						</Pressable>
-						<Pressable>
-							<FontAwesomeIcon icon={faCloudRain} size={15} />
-						</Pressable>
-						<Pressable>
-							<FontAwesomeIcon icon={faSnowflake} size={15} />
-						</Pressable>
-					</View>
-					<Modal
-						style={styles.modalCont}
-						animationType="none"
-						transparent={true}
-						visible={modalVisible}
-						onRequestClose={() => {
-							setModalVisible(!modalVisible);
-						}}
-					>
-						<DatePickerIOS date={currentDate} mode="date" onDateChange={handleSubmitDate}/>
-						<Pressable
-							onPress={() => {
-								setModalVisible(!modalVisible);
-							}}
-						>
-							<Text>Submit</Text>
-						</Pressable>
-					</Modal>
-					<Pressable
-						onPress={() => {
-							setModalVisible(true);
-						}}
-					>
-						<Text >Select Date</Text>
-					</Pressable>
-          {chosenDate ? <Text>{chosenDate}</Text> : null}
-          
-					<Button onPress={handleSaveLog} title="Save" />
+    <View>
+      {LogFormVisible ? (
+        <View>
+          <TextInput placeholder="Notes"></TextInput>
+          <TextInput keyboardType="number-pad" placeholder="DD/MM/YYYY"></TextInput>
+          <View>
+            <Text>Weather:</Text>
+            <Pressable>
+              <FontAwesomeIcon icon={faSun} size={15} />
+            </Pressable>
+            <Pressable>
+              <FontAwesomeIcon icon={faCloud} size={15} />
+            </Pressable>
+            <Pressable>
+              <FontAwesomeIcon icon={faCloudRain} size={15} />
+            </Pressable>
+            <Pressable>
+              <FontAwesomeIcon icon={faSnowflake} size={15} />
+            </Pressable>
+          </View>
 
-				</View>
-			) : null}
-			{LogsVisibile ? (
-				<View>
-					<View>
-						<Text>Wow that was a tough one!</Text>
-					</View>
-					<View>
-						<Button onPress={handleAddLog} title="Add Log" />
-					</View>
-				</View>
-			) : null}
-		</View>
-	);
+          <Button onPress={handleSaveLog} title="Save" />
+        </View>
+      ) : null}
+
+      {LogsVisibile ? (
+        <View>
+          <View>
+            <Text>Wow that was a tough one!</Text>
+          </View>
+          <View>
+            <Button onPress={handleAddLog} title="Add Log" />
+          </View>
+        </View>
+      ) : null}
+    </View>
+  );
 };
 
 export default AddLog;
